409625c264b499b0724a9bab5caf7b5d27cf784d
kalunge <muthomititus+github@gmail.com>
2022-08-17 11:25:24 +0300

    Add select options for certain cuisines

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,6 +3,7 @@ import { spicyFoods, getNewRandomSpicyFood } from '../data';
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [cuisine, setCuisine] = useState('ALL');
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
@@ -23,16 +24,38 @@ function SpicyFoodList() {
     setFoods(newFoods);
   };
 
-  const foodList = foods.map((food) => (
+  const handleSelect = (e) => {
+    setCuisine(e.target.value);
+  };
+
+   const foodsToDisplay = foods.filter((food) => {
+     if (cuisine === 'All') {
+       return true;
+     } else {
+       return food.cuisine === cuisine;
+     }
+   });
+
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+ 
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+
+      <select name="filter" onChange={handleSelect}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
     </div>
   );
 }
